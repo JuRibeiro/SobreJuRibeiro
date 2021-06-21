93e823bfc7c0acc37d28d2ccbe60d173f9f8fd1e
JuRibeiro <ju.ribeiro1208@gmail.com>
2021-06-21 08:48:07 -0300

    atualizando o blog

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let nome = document.querySelector("#nome")
+
+
+function nomeZoom ()
+{
+   nome.querySelector.width= '40px'
+   nome.querySelector.width= '20px'
+}
+
+function nomeNormal ()
+{
+    nome.querySelector.width= '15px'
+   nome.querySelector.width= '10px'
+}
